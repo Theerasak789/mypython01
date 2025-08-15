d5324fbed419e589d204cbdfc3124d0b780c58c5
Theerasak789 <bankkiiot2@gmail.com>
2025-08-15 11:46:20 +0700

    1

---
+++ text02.py
@@ -0,0 +1,9 @@
+# Single Line Comment
+print("Hellol.....") # wow wow wow 
+
+# Multi Line Comment
+"""ใช้ Triple Single Quote
+afadacdaasfafaf
+gafasdaddadasdda
+agafasdasaafadad"""
+
---
+++ text03.py
@@ -0,0 +1,18 @@
+# กำลังแสดงผลข้อมูล ทางหน้าจอ ใช้ฟังก์ชัน print()
+# sting
+print("Hello")
+print("Hi")
+print("""Hey""")
+print("""Hmm""")
+
+# number
+print(12345) # int
+print(454.458798) # float
+
+# boolean ตรรกะ มีแค่ True(จริง) และ False(เท็จ)
+print(True)
+print(False)
+
+# Expression นิพจน์ คือ อะไรก็ตามที่่มี process การทำงาน เช่น การคํานวณ,ตัวแปร,... 
+print(10 + 20 + 5)
+print(10 * 2 / 2)
