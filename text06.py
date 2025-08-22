c4fdd1327d939dc746b239b26103ecc983317ae7
Theerasak789 <bankkiiot2@gmail.com>
2025-08-22 10:29:06 +0700

    up 22/8/68

---
+++ text06.py
@@ -0,0 +1,28 @@
+# operator ตัวดำเนินการ คือ เครื่องหมายที่ใช้ในการประมวลผลข้อมูล 
+# 1. arithmetic operator เครื่องหมายคำนวณ 
+# (+,-,*,/,% mod,//,**)
+print(10+3)
+print(10-3)
+print(10*3)
+print(10/3)
+print(10%3) # หารเอาเศษ
+print(10//3) # หารเอาส่วน
+print(10**3)
+print('++++++++++++++++++++++++++++++')
+print(10 == 3)
+print(10 != 3)
+print('Bangna' >= 'Bangpoo')
+print('Thailand' < 'THAILAND')
+print('++++++++++++++++++++++++++++++')
+# 3. logical operator เครื่องหมาย ตรรกะ (and,or,not)
+# ข้อมูลที่จะใช้้กับเครื่องหมายนี้จะต้องเป็น true or false และผลลัพธ์ที่ได้จากการใช้เครื่องหมาย true or false   
+print(not True)
+print(not False)
+print('-------')
+print(True and True)#True
+print(True and False)
+print('--------')
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)#False
