92d426e424247e1b9057698186a0881e0da69023
Theerasak789 <bankkiiot2@gmail.com>
2025-09-05 12:00:06 +0700

    com3

---
+++ text24.py
@@ -0,0 +1,12 @@
+# Function คือ การทำงานหนึ่งๆ จะไม่ทำงานหากไม่เรียกใช้ (call fanction)
+
+# Function มีที่มาที่ไปยังบาง
+# - Standard Function / Built in Function มากับภาษาโปรแกรม
+# - User Defined Function ฟังก์ชันที่ Dev สร้างขึ้นเอง
+# - Third Party Function  ฟังก์ชันที่ คนอื่น ๆ สร้างขึ้นเอง
+
+# Function มี 4 ประเภท
+# - no parameter no return 
+# - has parameter no return
+# - no parameter have return
+# - have parameter have return
